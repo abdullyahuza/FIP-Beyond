899345631df054a73f65f7518470f333ba826b1c
abdullyahuza <yahuzaabdulrazak@gmail.com>
2025-05-13 20:23:54 +0100

    init

---
+++ week1/HelloWorld.java
@@ -0,0 +1,9 @@
+// This is a simple Java program that prints "Hello, World!" to the console.
+// Submitted by: Abdulrazak Yahuza
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
